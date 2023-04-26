505f0e4464f6b78add0fcef4ae04c7a9f78a3fa9
dev-nolan-ayer <dev.nolan.ayer@gmail.com>
2023-04-26 16:39:16 -0700

    modified css class values for responsive design

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,6 +89,7 @@ export default function Home() {
               id="targetImage"
               src={userImage}
               fill
+              style={{ objectFit: "cover" }}
             />
           </div>
         </div>

11067afd34ebdabd68d78ffbccd55eff43c561a1
dev-nolan-ayer <dev.nolan.ayer@gmail.com>
2023-04-26 22:54:29 -0700

    removed unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,12 +25,6 @@ export default function Home() {
     const colorResult = document.getElementById("colorResult");
     const hexResult = document.getElementById("hexResult");
 
-    const hexToRgb = (hex) => {
-      const r = parseInt(hex.slice(1, 3), 16);
-      const g = parseInt(hex.slice(3, 5), 16);
-      const b = parseInt(hex.slice(5), 16);
-    };
-
     const eyeDropper = new EyeDropper();
 
     eyeDropper
@@ -40,7 +34,6 @@ export default function Home() {
         fetchColorHandler(trimmedHex);
         hexResult.textContent = `HEX: ${result.sRGBHex}`;
         colorResult.style.backgroundColor = result.sRGBHex;
-        hexToRgb(result.sRGBHex);
       })
       .catch((e) => {
         colorResult.textContent = e;
